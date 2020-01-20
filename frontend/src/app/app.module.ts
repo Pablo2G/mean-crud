8a896ad8fe7cfa166900c6b2066bc20e7325916d
pablo2g <pablogamez.18@campuscamara.es>
2020-01-20 19:46:25 +0100

    angular_install_2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeachersComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ frontend/src/app/services/teachers.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Teacher } from 'app/models/teacher';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeachersService {
+
+  constructor() { }
+}
+
